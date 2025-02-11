054a3f7592417fe649b44680e736d6640004538f
Haokai Xuan <haokai.xuan2006@gmail.com>
2025-02-10 23:32:23 -0500

    stack app

--- LeetCode/Medium/RemAllOccSubstr.cpp
+++ LeetCode/Medium/RemAllOccSubstr.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        string curr = "";
-        int n = part.size();
+        stack<char> st;
 
         for (int i = 0; i < s.size(); i++) {
-            curr += s[i];
-            if (curr.find(part) != string::npos) {
-                for (int i = 0; i < n; i++) {
-                    curr.pop_back();
-                }
+            st.push(s[i]);
+            if (st.size() >= part.size() && check_match(st, part, part.length())) {
+                for (int j = 0; j < part.length(); j++) {
+                    st.pop();
+                }              
             }
         }
 
-        return curr;
+        string ans = "";
+        while (!st.empty()) {
+            ans = st.top() + ans;
+            st.pop();
+        }
+
+        return ans;
+    }
+
+private:
+    bool check_match(stack<char> &st, string &part, int length) {
+        stack<char> temp = st;
+
+        for (int i = length - 1; i >= 0; i--) {
+            if (temp.top() != part[i]) {
+                return false;
+            }
+
+            temp.pop();
+        }
+
+        return true;
     }
 };
