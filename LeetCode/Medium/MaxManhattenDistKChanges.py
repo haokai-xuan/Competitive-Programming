0eb50d8a07682ff386298ae3a8433aaafc3fd2fb
haokai-xuan <haokai.xuan2006@gmail.com>
2025-06-23 16:44:30 -0400

    maximum manhatten distance after k changes

---
+++ LeetCode/Medium/MaxManhattenDistKChanges.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        def get_axis_dist(dir_cnt, opp_dir_cnt, t):
+            return abs(dir_cnt - opp_dir_cnt) + t * 2
+
+        ans = 0
+        north = south = east = west = 0
+        for c in s:
+            if c == 'N':
+                north += 1
+            elif c == 'S':
+                south += 1
+            elif c == 'E':
+                east += 1
+            else:
+                west += 1
+
+            t1 = min(north, south, k)
+            t2 = min(east, west, k - t1)
+
+            ans = max(ans, get_axis_dist(north, south, t1) + get_axis_dist(east, west, t2))
+
+        return ans
