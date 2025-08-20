cc323842021d6b88e3e714de6cdef47331cfa6b5
haokai-xuan <haokai.xuan2006@gmail.com>
2025-08-20 15:00:33 -0300

    1768. Merge Strings Alternately

---
+++ LeetCode/Easy/MergeStringsAlternately.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int p1 = 0;
+        int p2 = 0;
+        string ans = "";
+        while (p1 < word1.size() && p2 < word2.size()) {
+            if (p1 == p2) {
+                ans += word1[p1];
+                p1++;
+            }
+            else {
+                ans += word2[p2];
+                p2++;
+            }            
+        }
+        while (p1 < word1.size()) {
+            ans += word1[p1];
+            p1++;
+        }
+        while (p2 < word2.size()) {
+            ans += word2[p2];
+            p2++;
+        }
+        return ans;
+    }
+};
