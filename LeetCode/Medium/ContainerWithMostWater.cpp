1af1d1cd98147c89bab61ccfea0a3d46aad6ada3
haokai-xuan <haokai.xuan2006@gmail.com>
2025-10-03 20:19:23 -0700

    11. Container With Most Water

--- LeetCode/Medium/ContainerWithMostWater.cpp
+++ LeetCode/Medium/ContainerWithMostWater.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
+        int left = 0, right = height.size() - 1;
         int ans = 0;
-
         while (left < right) {
-                            ans = max(ans, (right - left) * min(height[left], height[right]));
-            if (height[left] < height[right]) {
-                left++;
-            } else if (height[right] < height[left]) {
+            int width = right - left;
+            ans = max(ans,  width * min(height[left], height[right]));
+            if (height[left] > height[right]) {
                 right--;
             }
             else {
-                if (left + 1 < height.size() && right - 1 >= 0 && height[left + 1] > height[right - 1]) {
-                    right--;
-                }
-                else {
-                    left++;
-                }
+                left++;
             }
         }
-
         return ans;
     }
 };
