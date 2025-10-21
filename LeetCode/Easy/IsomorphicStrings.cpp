abde4b408870191a47cf275dd6944d122b6704e2
haokai-xuan <haokai.xuan2006@gmail.com>
2025-10-20 22:18:53 -0700

    205. Isomorphic Strings

--- LeetCode/Easy/IsomorphicStrings.cpp
+++ LeetCode/Easy/IsomorphicStrings.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> map_s_to_t;
-        map<char, char> map_t_to_s;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (map_s_to_t[s[i]] == NULL) {
-                map_s_to_t[s[i]] = t[i];
-            } else if (map_s_to_t[s[i]] != NULL) {
-                if (map_s_to_t[s[i]] != t[i]) {
+        unordered_map<char, char> mpST;
+        unordered_map<char, char> mpTS;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (mpST.count(s[i])) {
+                if (mpST[s[i]] != t[i])
                     return false;
-                }
             }
+            else
+                mpST[s[i]] = t[i];
 
-            if (map_t_to_s[t[i]] == NULL) {
-                map_t_to_s[t[i]] = s[i];
-            } else if (map_t_to_s[t[i]] != NULL) {
-                if (map_t_to_s[t[i]] != s[i]) {
+            if (mpTS.count(t[i])) {
+                if (mpTS[t[i]] != s[i])
                     return false;
-                }
             }
+            else
+                mpTS[t[i]] = s[i];
         }
         return true;
     }
