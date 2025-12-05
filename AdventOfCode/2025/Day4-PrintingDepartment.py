8136333bda256f2d0d1feaf7a7eed7fc13c6c9da
haokai-xuan <haokai.xuan2006@gmail.com>
2025-12-04 17:51:51 -0800

    3432. Count Partitions with Even Sum Difference

---
+++ AdventOfCode/2025/Day4-PrintingDepartment.py
@@ -0,0 +1,30 @@
+grid = []
+while True:
+    row = input()
+    if row == 'q':
+        break
+    grid.append(list(row))
+
+d = [(-1, -1), [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+
+rows, cols = len(grid), len(grid[0])
+ans = 0
+
+for i in range(rows):
+    for j in range(cols):
+        if grid[i][j] != "@":
+            continue
+        cnt = 0
+        valid = True
+        for dx, dy in d:
+            nr = i + dx
+            nc = j + dy
+            if nr >= 0 and nr < rows and nc >= 0 and nc < cols:
+                if grid[nr][nc] == "@":
+                    cnt += 1
+                    if cnt >= 4:
+                        valid = False
+        if valid:
+            ans += 1
+
+print(ans)
