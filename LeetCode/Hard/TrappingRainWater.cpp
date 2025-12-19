9d4d50f5e134ca2d71238033a9fe1b3b2936066f
haokai-xuan <haokai.xuan2006@gmail.com>
2025-12-18 21:56:22 -0800

    42. Trapping Rain Water

--- LeetCode/Hard/TrappingRainWater.cpp
+++ LeetCode/Hard/TrappingRainWater.cpp
@@ -2,24 +2,45 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> maxLeft(n);
-        maxLeft[0] = 0;
-        vector<int> maxRight(n);
-        maxRight[n - 1] = 0;
-        for (int i = 1; i < n; i++) {
-            maxLeft[i] = max(maxLeft[i - 1], height[i - 1]);
-            maxRight[n - i - 1] = max(maxRight[n - i], height[n - i]);
-        }
-        vector<int> minLR(n);
-        for (int i = 0; i < n; i++)
-            minLR[i] = min(maxLeft[i], maxRight[i]);
-
+        vector<int> left(n);
+        vector<int> right(n);
+        stack<int> st;
         int ans = 0;
         for (int i = 0; i < n; i++) {
-            int trapped = minLR[i] - height[i];
-            if (trapped > 0) ans += trapped;
+            if (st.empty()) {
+                st.push(height[i]);
+                left[i] = 0;
+            }
+            else if (height[i] <= st.top()) {
+                left[i] = st.top();
+            }
+            else {
+                left[i] = st.top();
+                st.push(height[i]);
+            }
         }
-
+        st = {};
+        for (int i = n - 1; i >= 0; i--) {
+            if (st.empty()) {
+                st.push(height[i]);
+                right[i] = 0;
+            }
+            else if (height[i] <= st.top()) {
+                right[i] = st.top();
+            }
+            else {
+                right[i] = st.top();
+                st.push(height[i]);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            int l = left[i], r = right[i];
+            int h = min(l, r) - height[i];
+            if (h > 0) {
+                ans += h;
+            }
+        }
         return ans;
     }
 };
