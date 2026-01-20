eba826a6e78e2846d2c9e830ebf91ae987b35c09
haokai-xuan <haokai.xuan2006@gmail.com>
2026-01-19 20:24:49 -0800

    3314. Construct the Minimum Bitwise Array I

---
+++ LeetCode/Easy/ConstructTheMinimumBitwiseAndArrayI.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            bool found = false;
+            for (int j = 1; j < nums[i]; j++) {
+                if ((j | (j + 1)) == nums[i]) {
+                    ans.push_back(j);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) ans.push_back(-1);
+        }
+
+        return ans;
+    }
+};
