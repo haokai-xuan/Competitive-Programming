82ae4913e997547167d7fe6c54c0ed6e1ac69610
haokai-xuan <haokai.xuan2006@gmail.com>
2026-01-30 19:44:30 -0800

    744. Find Smallest Letter Greater Than Target

--- LeetCode/Easy/FindSmallestLtrGreaterThanTarget.cpp
+++ LeetCode/Easy/FindSmallestLtrGreaterThanTarget.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        for (char ltr : letters){
-            if (ltr > target){
-                return ltr;
-            }
+        int left = 0;
+        int right = letters.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) left = mid + 1;
+            else right = mid;
         }
-        return letters[0];
+        return left == letters.size() ? letters[0] : letters[left];
     }
 };
