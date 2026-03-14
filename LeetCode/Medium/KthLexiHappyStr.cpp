6c2eeb0d186001059034577cf89b05cf6a3eb132
haokai-xuan <haokai.xuan2006@gmail.com>
2026-03-13 21:00:49 -0700

    1415. The k-th Lexicographical String of All Happy Strings of Length n

--- LeetCode/Medium/KthLexiHappyStr.cpp
+++ LeetCode/Medium/KthLexiHappyStr.cpp
@@ -1,35 +1,23 @@
 class Solution {
+    void generate(string s, int n, int idx, vector<string>& happy) {
+        if (s.size() == n) {
+            happy.push_back(s);
+            return;
+        }
+        for (char c : {'a', 'b', 'c'}) {
+            if (idx == 0 || s[idx - 1] != c) {
+                generate(s + c, n, idx + 1, happy);
+            }
+        }
+    }
 public:
     string getHappyString(int n, int k) {
-        string str = "";
-        int rank = 0;
-        solve(str, rank, n, k);
+        vector<string> happy;
 
-        return str;
-    }
+        generate("", n, 0, happy);
 
-private:
-    bool solve(string &curr_str, int &curr_rank, int n, int k) {
-        if (curr_str.size() == n) {
-            curr_rank++;
-            if (curr_rank == k) {
-                return true;
-            }
-            return false;
-        }
-
-        for (char c = 'a'; c <= 'c'; c++) {
-            if (!curr_str.empty() && curr_str.back() == c)
-                continue;
-
-            curr_str.push_back(c);
-            if (solve(curr_str, curr_rank, n, k)) {
-                return true;
-            }
-            curr_str.pop_back();
-        }
-        
-        // curr_str = "";
-        return false;
+        if (happy.size() < k) return "";
+        sort(happy.begin(), happy.end());
+        return happy[k - 1];
     }
 };
