618b4c2274bde4b9e22600da291a18836eca832e
haokai-xuan <haokai.xuan2006@gmail.com>
2026-09-09 20:44:38 -0300

    3871. Count Commas in Range II

--- LeetCode/Medium/CountCommasInRangeII.cpp
+++ LeetCode/Medium/CountCommasInRangeII.cpp
@@ -1,20 +1,14 @@
-class Solution {
-public:
-    long long countCommas(long long n) {
-        map<pair<long long, long long>, long long> mp = {
-            {{1, 999}, 0},
-            {{1000, 999999}, 1},
-            {{1000000, 999999999}, 2},
-            {{1000000000, 999999999999}, 3},
-            {{1000000000000, 999999999999999}, 4},
-            {{1000000000000000, 1000000000000000}, 5},
-        };
-        long long ans = 0;
-        for (auto& [p, m] : mp) {
-            // cout << p.first << ' ' << p.second << ' ' << m << '\n';
-            if (n < p.first) break;
-            ans += m * (min(p.second, n) - p.first + 1);
+    class Solution {
+    public:
+        long long countCommas(long long n) {
+            long long ans = 0;
+
+            if (n >= 1000) ans += n - 1000 + 1;
+            if (n >= 1000000) ans += n - 1000000 + 1;
+            if (n >= 1000000000) ans += n - 1000000000 + 1;
+            if (n >= 1000000000000) ans += n - 1000000000000 + 1;
+            if (n == 1000000000000000) ans += 1;
+
+            return ans;
         }
-        return ans;
-    }
-};
+    };
